7db07720bb2fbe39ae5fc6d1cbcc2df9d4b00e99
abdessamdalami <eloualialamiabdessamad@gmail.com>
2023-04-02 22:22:56 +0000

    good

--- srcs/requirements/mariadb/tools/dump.sql
+++
@@ -1,4 +0,0 @@
-[mysqld]
-'skip-host-cache'; \
-      echo 'skip-name-resolve'; \
-      echo 'bind-address=0.0.0.0'; \
--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,18 +1,5 @@
 #!bin/sh
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-
-        chown -R mysql:mysql /var/lib/mysql
-
-        # init database
-        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-
-        tfile=`mktemp`
-        if [ ! -f "$tfile" ]; then
-                return 1
-        fi
-fi
-
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
 
         cat << EOF > /tmp/create_db.sql
