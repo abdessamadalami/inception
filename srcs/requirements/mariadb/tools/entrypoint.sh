1b9a37f0ba97c58c2fafb624f792e7b47a906094
abdessamdalami <eloualialamiabdessamad@gmail.com>
2023-04-04 21:23:14 +0000

    is good to start ask q!!!!!

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,21 +1,8 @@
-#!bin/sh
-
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
-
-        cat << EOF > /tmp/create_db.sql
-USE mysql;
-FLUSH PRIVILEGES;
-DELETE FROM     mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
-FLUSH PRIVILEGES;
-EOF
-        # run init.sql
-        /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-        rm -f /tmp/create_db.sql
-fi
+#!/bin/sh
+service mysql start
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress ;"
+mysql -u root -e "CREATE USER IF NOT EXISTS alami@'localhost' IDENTIFIED BY 'alami';"
+mysql -u root -e "ALTER USER alami@'localhost' IDENTIFIED BY 'alami' ;"
+#more test about the new password 
+service  mysql stop
+mysqld
