92e02dbba125a76dad0a9db46a402b0d836fbdf7
abdessamdalami <eloualialamiabdessamad@gmail.com>
2023-04-05 05:45:19 +0000

    mariadb exited with code 1

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -8,8 +8,8 @@ else
 	#env variable 
 	sed -i "s/username_here/alami/g" wp-config-sample.php
 	sed -i "s/password_here/abd/g" wp-config-sample.php
-	sed -i "s/localhost/aaa/g" wp-config-sample.php
-	sed -i "s/database_name_here/aaaa/g" wp-config-sample.php
+	sed -i "s/localhost/alami/g" wp-config-sample.php
+	sed -i "s/database_name_here/alami/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 	wp core install  --url=ael-oual.com --title="Your Blog Title" --admin_name=wordpress_admin --admin_password=4Long --admin_email=you@example.com --allow-root
 fi
