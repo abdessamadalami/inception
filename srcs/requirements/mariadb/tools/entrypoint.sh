0509c9216b410bccff4141d3339ee0f2212c55f5
abdessamadalami <eloualialamiabdessamad@gmail.com>
2023-04-11 14:35:59 +0000

    good in with mysite

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,13 +1,16 @@
 #!bin/bash
-service mysql start;
-echo "CREATE DATABASE IF NOT EXISTS wordpress ;" > s.sql;
-#echo "CREATE USER IF NOT EXISTS '$USER'@'localhost' IDENTIFIED BY '$PASSWORD';" >> s.sql;
-mysql < s.sql ;
-#mysql -e "GRANT ALL PRIVILEGES ON '$DBNAME'.* TO '$USER'@'%';"
-#mysql -u root -p -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'alami1234' ;"
-#mysql -e "FLUSH PRIVILEGES;"
-#kill $(cat /var/run/mysqld/mysqld.pid)
-service mysql stop;
-
-
+if [ -f s.sql ]
+then
+	echo "configurate"
+else
+	service mysql start;
+	echo "starting ......."
+	echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > s.sql;
+	echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';" >> s.sql;
+	echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" >> s.sql;
+	mysql < s.sql ;
+	#mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'alami1234' ;"
+	#kill $(cat /var/run/mysqld/mysqld.pid)
+	echo "end conf ......."
+fi
 mysqld
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
+
+	#status=1
+	#while [ "$status" -ne "0" ]
+	#do
+	#	ping -c 1 mariadb
+	#	status=$?
+	#	sleep 1
+	#	echo $status
+	#done
+
 if [ -f ./wp-config.php ]
 then
 	echo "wp downloaded"
 else
 	#Download wordpress and all config file
 	wp core download --allow-root
-	#env variable 
-	sed -i "s/username_here/$USER/g" wp-config-sample.php
-	sed -i "s/password_here/$PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$DB_NAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$DB_NAME/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
-	wp core install  --url=ael-oual.com --title="alah ishal amrk" --admin_name=$USER --admin_password=$PASS --admin_email=$Email --allow-root
-	wp user create $USER $EMAIL --role=author --user_pass=$PASSWORD --allow-root
-fi
+	#env variable
+	echo "I am here in here";
+	sed -i "s/username_here/ael-oual/g" wp-config-sample.php
+	sed -i "s/password_here/pass/g" wp-config-sample.php
+	sed -i "s/localhost/mariadb/g" wp-config-sample.php
+	sed -i "s/database_name_here/wordpress/g" wp-config-sample.php
 
-exec "$@"
+
+	cp wp-config-sample.php wp-config.php
+
+
+	wp core install --url=example.com --title="My Site of my sitw " --admin_user=ael-oual --admin_password=ael-oual --admin_email=ael-oual@ael-oual.com --allow-root
+	wp user create alami alami@gmail.com --role=author --user_pass=pass --allow-root
+	echo "I am here in here";
+fi
+exec $@
