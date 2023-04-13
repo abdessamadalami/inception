998224722ea1bd9939b287d9cce92a960a3a1893
abdessamadalami <eloualialamiabdessamd@gmail.com>
2023-04-13 15:57:35 +0000

    good

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-
-	#status=1
-	#while [ "$status" -ne "0" ]
-	#do
-	#	ping -c 1 mariadb
-	#	status=$?
-	#	sleep 1
-	#	echo $status
-	#done
-
 if [ -f ./wp-config.php ]
 then
 	echo "wp downloaded"
@@ -16,18 +6,17 @@ else
 	#Download wordpress and all config file
 	wp core download --allow-root
 	#env variable
-	echo "I am here in here";
-	sed -i "s/username_here/ael-oual/g" wp-config-sample.php
-	sed -i "s/password_here/pass/g" wp-config-sample.php
+	sed -i "s/username_here/$DB_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$DB_PASS/g" wp-config-sample.php
 	sed -i "s/localhost/mariadb/g" wp-config-sample.php
-	sed -i "s/database_name_here/wordpress/g" wp-config-sample.php
+	sed -i "s/database_name_here/$DB_NAME/g" wp-config-sample.php
 
 
 	cp wp-config-sample.php wp-config.php
 
 
-	wp core install --url=example.com --title="My Site of my sitw " --admin_user=ael-oual --admin_password=ael-oual --admin_email=ael-oual@ael-oual.com --allow-root
-	wp user create alami alami@gmail.com --role=author --user_pass=pass --allow-root
-	echo "I am here in here";
+	wp core install --url=$DOMAIN_NAME --title="My Site " --admin_user=$WP_ADMIN --admin_password=$ADMIN_PASSWORD --admin_email=ael-oual@ael-oual.com --allow-root
+	wp user create $USER  $EMAIL --role=author --user_pass=$USER_PASSWORD --allow-root
+
 fi
 exec $@
